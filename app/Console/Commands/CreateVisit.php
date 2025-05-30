ea3aaa3dcefa2927661822d9f2b57507a62d9789
Stiven Torres <torresmendozastiven@gmail.com>
2025-05-29 22:28:20 -0500

    feat(api): mejorando la estructura del prompt y validaciones

--- app/Console/Commands/CreateVisit.php
+++ app/Console/Commands/CreateVisit.php
@@ -5,15 +5,17 @@ namespace App\Console\Commands;
 use App\Models\User;
 use App\Models\Visit;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Validator;
 use function Laravel\Prompts\text;
 class CreateVisit extends Command
 {
     /**
+     * 10.955961-74.7799936
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'app:create-visit';
+    protected $signature = 'visit:create';
 
     /**
      * The console command description.
@@ -27,11 +29,31 @@ class CreateVisit extends Command
      */
     public function handle()
     {
-        $name = text('Enter the name of the visit');
-        $email = text('Enter the email of the visit');
-        $latitude = text('Enter the latitude of the visit');
-        $longitude = text('Enter the longitude of the visit');
-        $identification = text('Enter the identification of the user');
+        $name = text(
+            label:'Enter the name of the visit',
+            placeholder:'Stiven Torres',
+            validate:['name' =>'required|max:255']
+        );
+        $email = text(
+            label:'Enter the email of the visit',
+            placeholder:'stiventorres@example.com',
+            validate:['email' =>'required|max:255|email']
+        );
+        $latitude = text(
+            label:'Enter the latitude of the visit',
+            placeholder:'10.955961',
+            validate:['latitude' =>'required|numeric|between:-90,90']
+        );
+        $longitude = text(
+            label:'Enter the longitude of the visit',
+            placeholder:'-74.7799936',
+            validate:['longitude' =>'required|numeric|between:-180,180']
+        );
+        $identification = text(
+            label:'Enter the identification of the user',
+            placeholder:'123456789',
+            validate:['identification' =>'required|max:255']
+        );
 
 
         $user = User::where('identification', $identification)->first();
